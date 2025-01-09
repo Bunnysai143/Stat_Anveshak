153f289282f19795802ebc148526b6ffeb89be93
saikiranmain1708@gmail.com <saikiranmain1708@gmail.com>
2025-01-09 17:19:09 +0530

    changing version

--- src/utils/colorUtils.js
+++
@@ -1,8 +0,0 @@
-// utils/colorUtils.js
-export const getWeb3Color = (index) => {
-    const colors = [
-      "#FF5733", "#33FF57", "#3357FF", "#F1C40F", "#9B59B6", "#1ABC9C"
-    ]; // Customize the color array as needed
-    return colors[index % colors.length]; // Cycle through colors
-  };
-  
